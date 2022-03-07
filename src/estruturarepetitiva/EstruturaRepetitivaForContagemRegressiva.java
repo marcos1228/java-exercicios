4f608b7db8c801b07e601c14e39c85ac64f23cbf
Marcos Barbosa <marcos_pp.16@hotmail.com>
2022-03-07 14:48:14 -0300

    Estrutura de repitição for(para) contagem regressiva

---
+++ src/estruturarepetitiva/EstruturaRepetitivaForContagemRegressiva.java
@@ -0,0 +1,11 @@
+package estruturarepetitiva;
+
+public class EstruturaRepetitivaForContagemRegressiva {
+	public static void main(String[] args) {
+		for(int i=4; i>=0;i--) {
+			System.out.println("Valor de i: " +i);
+			
+		}
+	}
+
+}
